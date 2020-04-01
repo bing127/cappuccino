e96d4c229aacd57e0713d6239625c7fb7783ac96
wxb <wxbing1207@163.com>
2020-04-01 14:13:15 +0800

    'model'

--- db/sys_user.go
+++ db/sys_user.go
@@ -1,23 +1,26 @@
 package db
 
+import "time"
+
 // User 用户实体
 type SysUser struct {
-	ID       string  `gorm:"column:id;primary_key;size:64;index;"`        // 记录内码
-	UserName *string `gorm:"column:user_name;size:64;index;"` // 用户名
-	RealName *string `gorm:"column:real_name;size:64;index;"` // 真实姓名
-	Password *string `gorm:"column:password;size:40;"`        // 密码(sha1(md5(明文))加密)
-	Email    *string `gorm:"column:email;size:255;index;"`    // 邮箱
-	Phone    *string `gorm:"column:phone;size:20;index;"`     // 手机号
-	Status   *int    `gorm:"column:status;index;"`            // 状态(1:启用 2:停用)
-	Creator  *string `gorm:"column:creator;size:36;"`         // 创建者
-	Model
+	ID         string     `gorm:"column:id;primary_key;size:64;index;"` // 记录内码
+	UserName   *string    `gorm:"column:user_name;size:64;index;"`      // 用户名
+	RealName   *string    `gorm:"column:real_name;size:64;index;"`      // 真实姓名
+	Password   *string    `gorm:"column:password;size:40;"`             // 密码(sha1(md5(明文))加密)
+	Email      *string    `gorm:"column:email;size:255;index;"`         // 邮箱
+	Phone      *string    `gorm:"column:phone;size:20;index;"`          // 手机号
+	Status     *int       `gorm:"column:status;index;"`                 // 状态(1:启用 2:停用)
+	CreateBy   *string    `gorm:"column:create_by;size:64;"`            // 创建者
+	CreateDate *string    `gorm:"column:create_date;size:64;"`          // 创建时间
+	UpdateBy   *time.Time `gorm:"column:update_by;"`                    // 更新者
+	UpdateDate *time.Time `gorm:"column:update_date;"`                  // 更新时间
 }
 
 // TableName 表名
 func (a SysUser) TableName() string {
 	return a.Model.TableName("sys_user")
 }
-
 
 // UserRole 用户角色关联实体
 type SysUserRole struct {
@@ -30,4 +33,3 @@ type SysUserRole struct {
 func (a SysUserRole) TableName() string {
 	return a.Model.TableName("sys_user_role")
 }
-
